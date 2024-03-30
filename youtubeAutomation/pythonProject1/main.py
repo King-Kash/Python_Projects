90c20f706803b87053eb910204792bb1b29f44d7
King-Kash <kashyap.kanumuri@gmail.com>
2024-03-30 13:17:39 -0700

    update

--- youtubeAutomation/pythonProject1/main.py
+++ youtubeAutomation/pythonProject1/main.py
@@ -2,10 +2,12 @@ from pytube import YouTube
 from sys import argv
 
 #argv[0] is always the name of the program and then argv[1] will be the link
-link = "https://www.youtube.com/watch?v=vEQ8CXFWLZU"
+link = argv[1]
 yt = YouTube(link)
 
 print("title: ", yt.title)
 print("views: ", yt.views)
+yd = yt.streams.get_highest_resolution()
+yd.download('/Users/kashyapkanumuri/Downloads/YouTube-Downloads')
 
 
