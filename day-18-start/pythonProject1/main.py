a05bdac8f4134ccd8eedefc421954a526022c561
King-Kash <kashyap.kanumuri@gmail.com>
2025-04-26 03:36:54 -0700

    update

--- day-18-start/pythonProject1/main.py
+++ day-18-start/pythonProject1/main.py
@@ -61,3 +61,40 @@ tim_orientation = tim.heading()
 
 screen = Screen()
 screen.exitonclick()
+
+
+
+import torch
+from transformers import AutoModelForCausalLM, AutoTokenizer, AwqConfig
+
+model_id = "hugging-quants/Meta-Llama-3.1-405B-Instruct-AWQ-INT4"
+quantization_config = AwqConfig(
+    bits=4,
+    fuse_max_seq_len=512, # Note: Update this as per your use-case
+    do_fuse=True,
+)
+
+tokenizer = AutoTokenizer.from_pretrained(model_id)
+model = AutoModelForCausalLM.from_pretrained(
+  model_id,
+  torch_dtype=torch.float16,
+  low_cpu_mem_usage=True,
+  device_map="auto",
+  quantization_config=quantization_config
+)
+
+prompt = [
+  {"role": "system", "content": "You are a helpful assistant, that responds as a pirate."},
+  {"role": "user", "content": "What's Deep Learning?"},
+]
+inputs = tokenizer.apply_chat_template(
+  prompt,
+  tokenize=True,
+  add_generation_prompt=True,
+  return_tensors="pt",
+  return_dict=True,
+).to("cuda")
+
+outputs = model.generate(**inputs, do_sample=True, max_new_tokens=256)
+print(tokenizer.batch_decode(outputs[:, inputs['input_ids'].shape[1]:], skip_special_tokens=True)[0])
+
